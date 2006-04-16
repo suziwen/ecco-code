628a304275709ecd9d99d882ceca6f97e5720f43
feanndor <feanndor@b34d6fe1-2f0f-0410-aa1c-d2a5b680012a>
2006-04-16 01:51:50 +0000

    -muitas alteracoes em tudo que envolve fileman

--- ecco/scripts/content.js
+++ ecco/scripts/content.js
@@ -8,6 +8,7 @@ Content = function() {
 	
 	// get all static xml content for menus and error messages
 	this.initialize = function() {
+		// talvez juntar todas as mensagens dentro do ecco.xml ao inves de colocar separado
 		ajax.get(cfg['docFilemanContent'],
 				{ async:false, 
 				  onEnd:'content.set("fileman", xmlDoc);', 
@@ -29,16 +30,15 @@ Content = function() {
 				  onError:'alert(cfg["msgEccoError"])' })		
 	}
 		
-	// set messages retrieved from initialize() to local vars
+
 	this.set = function(mod, xmlDoc) {
 		messages[mod] = xmlDoc;
 		ecco.set(mod);
 	}
-	// display error messages 
-	this.error = function(mod, id) {
-		var obj;
-		var out;
-		obj = messages[mod];
+
+	this.getMessage = function(mod, id) {
+		var out = '';
+		var obj = messages[mod];
 		obj = obj.documentElement.getElementsByTagName('messages')[0];
 		obj = obj.getElementsByTagName('msg');
 
@@ -47,13 +47,45 @@ Content = function() {
 				out = obj[i].firstChild.nodeValue;
 			}
 		}
+		return out;
+	}
 
+	
+	this.error = function(mod, id) {
+		var out = getMessage(mod, id);
 		if(arguments[2]) out = out.replace('\['+id+'\]',arguments[2]);
 		document.getElementById('messages').innerHTML = out;
 	}
 	
+	this.confirmation = function(mod, id, param) {
+		var out = this.getMessage(mod, id);
+		for(var i=0;i<param.length;i++) {
+			out = out.replace('\['+param[i]['name']+'\]',param[i]['value'])
+		}
+		out+='<button>'+this.getMessage(mod, 'cancelButton')+'</button>';
+		out+='<button>'+this.getMessage(mod, 'okButton')+'</button>';
+		alert(out)
+		document.getElementById('confirmation').innerHTML = out;
+		this.invert('confirmation','block');
+		
+	}
+	
+	this.getMenuItems = function(type) {
+		var out = '';
+		var obj = messages['fileman'];
+		obj = obj.documentElement.getElementsByTagName('menu')[0];
+		obj = obj.getElementsByTagName(type)[0];		
+		obj = obj.getElementsByTagName('item');
+		for(var i=0; i<obj.length; i++) {
+			out += '<a href="javascript:void(0)" onclick="'+obj[i].getAttribute('action')+'" onmouseover="clearTimeout(timeoutId)">'+obj[i].firstChild.nodeValue+'</a>';
+		}
+		return out;
+		
+	}
+	
 	this.invert = function(id) {
-		document.getElementById(id).style.display = (document.getElementById(id).style.display!='block') ? 'block' : 'none';
+		if(arguments[1]) document.getElementById(id).style.display = arguments[1];
+		else document.getElementById(id).style.display = (document.getElementById(id).style.display!='block') ? 'block' : 'none';
 	}
 	
 }
