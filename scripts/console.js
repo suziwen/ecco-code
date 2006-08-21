529f49384dc9e216961c1477f59c456e935a2c52
iogui <iogui@b34d6fe1-2f0f-0410-aa1c-d2a5b680012a>
2006-08-21 09:18:07 +0000

    Alterações para corrigir bug do cd

--- scripts/console.js
+++ scripts/console.js
@@ -2,18 +2,30 @@
 Console = function() {
 	Console = this;
 	var out = String;
+	var userpath = String;
+	var currentpath = String;
+	var lastpath = String;
 	
 	this.initialize = function() {
 		out = '';
 		$('output').readOnly = true;
+		userpath = cfg['path'] + '/users/'  + cfg['user'];
+		currentpath = userpath;
+		lastpath = currentpath;
 	}
 	
 	this.execute = function() {
 	
-		var command = $('command').value; 
+		var command = $('command').value;
+		var currentPathEncoded = currentpath.replace(/\//g, '%2F');
+		var userPathEncoded = userpath.replace(/\//g, '%2F');
+		var lastPathEncoded = lastpath.replace(/\//g, '%2F');
+		
+		//alert(userPathEncoded);
 
+		alert(lastpath);
 		AJAX.get(cfg['docConsole'], {
-				parameters:'action=execute&command='+command,
+				parameters:'action=execute&command='+command+'&currentpath='+currentPathEncoded+'&userpath='+userPathEncoded+'&lastpath='+lastPathEncoded,
 				onEnd:'Console.parse(xmlDoc.documentElement);', 
 				onError:'Content.showMessage("console","fileNotFoundError",cfg["docConsole"])' 
 				})
@@ -24,6 +36,14 @@ Console = function() {
 	this.parse = function(obj) { 
 		// trocar aqui por append de DOM e tirar o innerHTML
 //		out += 
+		// TODO: descobrir como obter o currentpath da tag <currentpath>
+		
+		var currentPathNode = obj.getElementsByTagName('currentpath')[0];
+		var lastPathNode = obj.getElementsByTagName('lastpath')[0];
+		//alert(currentPathNode);
+		currentpath = currentPathNode.firstChild.nodeValue;
+		lastpath = lastPathNode.firstChild.nodeValue;
+		alert(lastpath);
 		$('output').innerHTML += obj.firstChild.nodeValue;
 		$('output').scrollTop = $('output').scrollHeight;
 		Fileman.update()
