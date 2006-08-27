b1af415e1daa0b67dea1605f620389eca80754f9
feanndor <feanndor@b34d6fe1-2f0f-0410-aa1c-d2a5b680012a>
2006-08-27 02:41:25 +0000

    beginning integration of new editor with syntax highlighting

--- ecco/servlet/Editor.java
+++ ecco/servlet/Editor.java
@@ -10,6 +10,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
+import java.util.regex.*;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -27,15 +28,16 @@ public class Editor extends HttpServlet {
     	out = getOutStream(response);
     	String action = request.getParameter("action");
 
-    	response.setContentType("text/xml");
-        out.write("<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?>\n");
+    	response.setContentType("text/html");
+        //out.write("<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?>\n"); 
    	
     	if(action == null) {
     		this.error();
     	}
     	else if(action.equals("open")) {
     		String file = request.getParameter("file");
-          	this.open(file);
+    		String editor = request.getParameter("editor");
+          	this.open(file, editor);
     	}
     	else if(action.equals("save")) {
     		String file = request.getParameter("file");
@@ -65,19 +67,39 @@ public class Editor extends HttpServlet {
     }
     
     
-    public void open(String file) {
+    public void open(String file, String editor) {
 	 	
     	try {
     			File f = new File(usersPath+login+File.separator+file);
     		    // verificar aqui se é arquivo binario e dar erro
     	        BufferedReader in = new BufferedReader(new FileReader(f));
     	        String str;
-    	        
-    			out.write("<text><![CDATA[");
-    	        while ((str = in.readLine()) != null) {
-    	            out.write(str+"\r\n");
-    	        }
-    	        out.write("]]></text>");
+			
+				if(editor.equals("rich")) { // rich editor
+					out.write("<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\"><html><head><link type=\"text/css\" rel=\"stylesheet\" href=\"/ecco/styles/rtsh-java.css\" /><script type=\"text/javascript\" src=\"/ecco/scripts/rtsh.js\"></script><script type=\"text/javascript\">RTSH.language = 'java';</script></head>");
+					out.write("<body contenteditable='true' id='edt'><PRE>");
+				    Pattern lt = Pattern.compile("<");
+				    Pattern gt = Pattern.compile(">");
+				    Matcher m1;
+				    Matcher m2;
+				    //Matcher gtm = gt.matcher("&gt;");
+				    
+	    	        while ((str = in.readLine()) != null) {
+	    	        	m1 = lt.matcher(str);
+	    	        	str = m1.replaceAll("&lt;");
+	    	        	m2 = gt.matcher(str);
+					    str = m2.replaceAll("&gt;");
+	    	        	out.write(str+"\r\n");
+	    	        }
+					out.write("</PRE></body></html>");
+				}
+				else { // simple textarea editor
+	    			out.write("<text><![CDATA[");
+	    	        while ((str = in.readLine()) != null) {
+	    	            out.write(str+"\r\n");
+	    	        }
+	    	        out.write("]]></text>");
+				}
     	        in.close();
     	    } 
     	 catch (IOException e) {
